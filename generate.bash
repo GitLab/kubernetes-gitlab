692c860bf10116a921ba2d08dbd0072cfeb97255
Kamil Trzcinski <ayufan@ayufan.eu>
2017-01-14 09:17:40 -0500

    WIP

---
+++ generate.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+scope_files() {
+  for file in $@; do
+    cat "$file"
+    echo "---"
+  done
+}
+
+scope_files \
+  gitlab/redis-* \
+  gitlab/postgresql-* \
+  gitlab/gitlab-* \
+  gitlab-runner/gitlab-runner-* > gitlab-full.yml
+
+scope_files ingress/* > gitlab-ingress.yml
